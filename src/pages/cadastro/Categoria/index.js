37f085fde94f89801c359999d6bda5aa991900db
claramoreira <51670485+claramoreira@users.noreply.github.com>
2020-08-01 11:54:11 -0300

    server update

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -31,8 +31,9 @@ function CadastroCategoria() {
   // ============
 
   useEffect(() => {
-    if(window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias'; 
+      const URL = window.location.hostname.includes('localhost')
+        ? 'http://localhost:8080/categorias'
+        : 'https://clafix.herokuapp.com/categorias';
       fetch(URL)
        .then(async (respostaDoServer) =>{
         if(respostaDoServer.ok) {
@@ -43,7 +44,7 @@ function CadastroCategoria() {
         throw new Error('Não foi possível pegar os dados');
        })
     }    
-  }, []);
+  , []);
 
   return (
     <PageDefault>
